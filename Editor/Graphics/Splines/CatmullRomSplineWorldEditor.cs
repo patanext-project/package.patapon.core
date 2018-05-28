701e6da95e78f1763563ae8339309aa9a4d06bce
Guerro323 <kaltobattle@gmail.com>
2018-05-28 17:16:43 +0200

    Up to date files for the patapon core package

--- Editor/Graphics/Splines/CatmullRomSplineWorldEditor.cs
+++ Editor/Graphics/Splines/CatmullRomSplineWorldEditor.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using P4.Core.Graphics;
 using UnityEditor;
 using P4Main.Graphics;
 using Unity.Collections;
