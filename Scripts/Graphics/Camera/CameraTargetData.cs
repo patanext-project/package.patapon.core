b2d1c4bb77a828fdb2ff0db95f7b1c8c08e811c2
Guerro323 <kaltobattle@gmail.com>
2018-08-18 18:35:22 +0200

    Moved the camera target component into main package

--- Scripts/Graphics/Camera/CameraTargetData.cs
+++ Scripts/Graphics/Camera/CameraTargetData.cs
@@ -1,39 +1,6 @@
-﻿using Unity.Entities;
-using Unity.Mathematics;
 
 namespace package.patapon.core
 {
-    public struct CameraTargetData : IComponentData
-    {
-        /// <summary>
-        /// The camera target
-        /// </summary>
-        public Entity CameraId;
-        /// <summary>
-        /// The priority of our work
-        /// </summary>
-        public int Priority;
-
-        public CameraTargetData(Entity cameraId, int priority)
-        {
-            CameraId = cameraId;
-            Priority = priority;
-        }
-    }
-    
-    public struct CameraTargetPosition : IComponentData
-    {
-        /// <summary>
-        /// The target position
-        /// </summary>
-        public float2 Value;
-
-        public CameraTargetPosition(float2 position)
-        {
-            Value = position;
-        }
-    }
-
     public struct CameraTargetAnchor : IComponentData
     {
         /// <summary>
