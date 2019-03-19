af3717b575678a7d9ea9ef67e4bd64a0375c812c
Guerro323 <kaltobattle@gmail.com>
2019-03-19 11:57:56 +0100

    The taterazay sample is now using DOTS physic

--- MoveToDefault/TestTaterazay.cs
+++ MoveToDefault/TestTaterazay.cs
@@ -1,6 +1,9 @@
 using package.StormiumTeam.GameBase;
 using StormiumTeam.GameBase;
 using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Physics;
+using Unity.Transforms;
 
 namespace Patapon4TLB.Default
 {
@@ -8,20 +11,63 @@ namespace Patapon4TLB.Default
 	{
 		protected override void OnStartRunning()
 		{
-			var taterazay = EntityManager.CreateEntity
+			var cpCollider = CapsuleCollider.Create(float3.zero, new float3(0, 2, 0), 0.5f);
+			var characterEntity = EntityManager.CreateEntity
 			(
+				// default components for movables...
+				typeof(Translation),
+				typeof(Rotation),
+
+				// default components for physics movables...
+				typeof(PhysicsCollider),
+				typeof(PhysicsMass),
+				typeof(PhysicsVelocity),
+				typeof(PhysicsGravityFactor),
+
+				// default components for characters...
 				typeof(LivableDescription),
+				typeof(ActionContainer),
+
+				// kit custom components...
 				typeof(TaterazayKitDescription),
 				typeof(TaterazayKitBehaviorData),
 				typeof(UnitDirection),
 				typeof(UnitBaseSettings),
-				typeof(Velocity),
-				typeof(ActionContainer),
 				typeof(RhythmActionController)
 			);
 
-			EntityManager.SetComponentData(taterazay, new UnitDirection {Value        = 1});
-			EntityManager.SetComponentData(taterazay, new UnitBaseSettings {BaseSpeed = 6});
+			// ...
+			EntityManager.SetComponentData(characterEntity, new Translation
+			{
+				Value = new float3(0, 2, 0)
+			});
+			EntityManager.SetComponentData(characterEntity, new Rotation
+			{
+				Value = quaternion.identity
+			});
+			// ...
+			EntityManager.SetComponentData(characterEntity, new PhysicsCollider
+			{
+				Value = cpCollider
+			});
+			EntityManager.SetComponentData(characterEntity, PhysicsMass.CreateKinematic(cpCollider.Value.MassProperties));
+			EntityManager.SetComponentData(characterEntity, new PhysicsVelocity
+			{
+				Linear = new float3(1, 0, 0), Angular = float3.zero
+			});
+			EntityManager.SetComponentData(characterEntity, new PhysicsGravityFactor
+			{
+				Value = 0f // kinematic body are not affected by normal gravity...
+			});
+			// ...
+			EntityManager.SetComponentData(characterEntity, new UnitDirection
+			{
+				Value = 1
+			});
+			EntityManager.SetComponentData(characterEntity, new UnitBaseSettings
+			{
+				BaseSpeed = 6
+			});
 
 			var marchAction = EntityManager.CreateEntity
 			(
@@ -30,7 +76,7 @@ namespace Patapon4TLB.Default
 				typeof(TaterazayKitMarchAction.Settings)
 			);
 
-			EntityManager.ReplaceOwnerData(marchAction, taterazay);
+			EntityManager.ReplaceOwnerData(marchAction, characterEntity);
 		}
 
 		protected override void OnUpdate()
