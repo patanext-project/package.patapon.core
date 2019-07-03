66206c252a8416b163a60bb2eebe6110c5d42829
Guerro323 <kaltobattle@gmail.com>
2019-07-03 18:30:31 +0200

    Player names are now correctly shown in UI

--- Data/UI/InGame/Multiplayer/UIPlayerDisplayNamePresentation.cs
+++ Data/UI/InGame/Multiplayer/UIPlayerDisplayNamePresentation.cs
@@ -1,5 +1,7 @@
+using P4.Core.Code.Networking;
 using Patapon4TLB.Core;
 using StormiumTeam.GameBase;
+using TMPro;
 using Unity.Entities;
 using Unity.NetCode;
 using Unity.Transforms;
@@ -9,12 +11,12 @@ namespace Patapon4TLB.UI.InGame
 {
 	public class UIPlayerDisplayNamePresentation : RuntimeAssetPresentation<UIPlayerDisplayNamePresentation>
 	{
-		
+		public TextMeshProUGUI NameLabel;
 	}
 
 	public class UIPlayerDisplayNameBackend : RuntimeAssetBackend<UIPlayerDisplayNamePresentation>
 	{
-		
+		public NativeString64 PreviousName;
 	}
 	
 	[UpdateInGroup(typeof(ClientPresentationSystemGroup))]
@@ -33,6 +35,20 @@ namespace Patapon4TLB.UI.InGame
 					-0.3f,
 					0
 				);
+
+				var presentation = backend.Presentation;
+				if (presentation == null)
+					return;
+
+				var playerRelative = EntityManager.GetComponentData<Relative<PlayerDescription>>(backend.DstEntity).Target;
+				if (playerRelative == default || !EntityManager.HasComponent<PlayerName>(playerRelative))
+					return;
+
+				var nativeStr = EntityManager.GetComponentData<PlayerName>(playerRelative).Value;
+				if (backend.PreviousName.Equals(nativeStr))
+					return;
+
+				presentation.NameLabel.text = nativeStr.ToString();
 			});
 		}
 	}
