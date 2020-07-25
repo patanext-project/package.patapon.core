2db6f3d854e1cd0b1bef2b0f60ba2098488c53bd
Guerro323 <kaltobattle@gmail.com>
2020-07-25 23:37:58 +0200

    match gamehost data

--- Client/Replicated/GamePlay/RhythmEngine/RhythmEnginePredictedCommandBuffer.cs
+++ Client/Replicated/GamePlay/RhythmEngine/RhythmEnginePredictedCommandBuffer.cs
@@ -1,12 +1,14 @@
 ﻿﻿using GameHost.Simulation.Features.ShareWorldState.BaseSystems;
+ using GameHost.Simulation.Utility.Resource;
  using PataNext.Module.Simulation.Components.GamePlay.RhythmEngine.Structures;
+ using PataNext.Module.Simulation.Resources;
  using Unity.Entities;
 
  namespace PataNext.Module.Simulation.Components.GamePlay.RhythmEngine
 {
 	public struct RhythmEnginePredictedCommandBuffer : IBufferElementData
 	{
-		public FlowPressure Value;
+		public GameResource<RhythmCommandResource> Value;
 
 		public class Register : RegisterGameHostComponentBuffer<RhythmEnginePredictedCommandBuffer>
 		{
--- Client/Replicated/Resources/Keys/RhythmCommandResourceKey.cs
+++ Client/Replicated/Resources/Keys/RhythmCommandResourceKey.cs
@@ -6,35 +6,43 @@ using GameHost.Simulation.Utility.Resource.Components;
  using Unity.Entities;
 
  [assembly: RegisterGenericComponentType(typeof(GameResourceKey<RhythmCommandResourceKey>))]
- 
-namespace PataNext.Module.Simulation.Resources.Keys
-{
-	public readonly struct RhythmCommandResourceKey : IGameResourceKeyDescription, IEquatable<RhythmCommandResourceKey>
-	{
-		public readonly CharBuffer64 Value;
 
-		public class Register : RegisterGameHostComponentData<GameResourceKey<RhythmCommandResourceKey>>
-		{
-		}
-		
-		public RhythmCommandResourceKey(CharBuffer64 value) => Value = value;
-		public RhythmCommandResourceKey(string       value) => Value = CharBufferUtility.Create<CharBuffer64>(value);
-		
-		public static implicit operator RhythmCommandResourceKey(string value) => new RhythmCommandResourceKey(value);
+ namespace PataNext.Module.Simulation.Resources.Keys
+ {
+	 public readonly struct RhythmCommandResourceKey : IGameResourceKeyDescription, IEquatable<RhythmCommandResourceKey>
+	 {
+		 public readonly CharBuffer64 Identifier;
+		 public readonly int          BeatDuration;
 
-		public bool Equals(RhythmCommandResourceKey other)
-		{
-			return Value.Equals(other.Value);
-		}
+		 public class Register : RegisterGameHostComponentData<GameResourceKey<RhythmCommandResourceKey>>
+		 {
+		 }
 
-		public override bool Equals(object obj)
-		{
-			return obj is RhythmCommandResourceKey other && Equals(other);
-		}
+		 public RhythmCommandResourceKey(CharBuffer64 identifier, int beatDuration)
+		 {
+			 Identifier   = identifier;
+			 BeatDuration = beatDuration;
+		 }
 
-		public override int GetHashCode()
-		{
-			return Value.GetHashCode();
-		}
-	}
-}
+		 public RhythmCommandResourceKey(string value, int beatDuration) : this(CharBufferUtility.Create<CharBuffer64>(value), beatDuration)
+		 {
+		 }
+
+		 public static implicit operator RhythmCommandResourceKey((string value, int beatDuration) tuple) => new RhythmCommandResourceKey(tuple.value, tuple.beatDuration);
+
+		 public bool Equals(RhythmCommandResourceKey other)
+		 {
+			 return Identifier.Equals(other.Identifier);
+		 }
+
+		 public override bool Equals(object obj)
+		 {
+			 return obj is RhythmCommandResourceKey other && Equals(other);
+		 }
+
+		 public override int GetHashCode()
+		 {
+			 return Identifier.GetHashCode();
+		 }
+	 }
+ }
